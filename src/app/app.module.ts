28687e1a2f3fd7739307b156fba39a32a862f500
David Richard <david@vitrio.com.br>
2018-12-11 19:47:09 -0200

    Alterações estrutura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./component/home/home.component";
+import { HeaderComponent } from "./component/shared/header/header.component";
+import { FooterComponent } from "./component/shared/footer/footer.component";
+import { HomeModule } from "./component/home/home.module";
+
+const MODULES = [HomeModule];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, ...MODULES],
+  exports: [...MODULES],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
